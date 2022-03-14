df09866d6e17c42bb59a16a81b60f72a3cab73eb
Chirag Panchal <Chiragpanchal1991@gmail.com>
2022-03-14 21:24:17 +0530

    Added delete and update command and added exceptions

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(p => p.Id)
+                .NotEmpty().WithMessage("{Id} is required.")
+                .NotNull();
+        }
+    }
+}
