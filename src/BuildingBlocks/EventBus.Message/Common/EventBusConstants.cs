0ee658ace3066d90d2816f26ccd47a8421b60a19
Chirag Panchal <Chiragpanchal1991@gmail.com>
2022-03-30 23:57:00 +0530

    Add EventBus.Message class library project

---
+++ src/BuildingBlocks/EventBus.Message/Common/EventBusConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace EventBus.Message.Common
+{
+    public static class EventBusConstants
+	{
+		/// <summary>
+		/// this used as basket checkout queue name and appear on rabbitMQ console
+		/// </summary>
+		public const string BasketCheckoutQueue = "basketcheckout-queue";
+	}
+}
